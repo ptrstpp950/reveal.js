84c6e46ca9b187504bc89ad43d94e9008ffac692
Piotr Stapp <ptrstpp950@gmail.com>
2014-02-12 09:31:28 +0100

    Added run.ps1 to fast lunch presentation with iisexpress

---
+++ run.ps1
@@ -0,0 +1,15 @@
+$path=$null;
+if(Test-Path -Path "c:\Program Files\IIS Express")
+{
+    $path = "c:\Program Files\IIS Express";
+}
+if(Test-Path -Path "c:\Program Files (x86)\IIS Express")
+{
+   $path = "c:\Program Files (x86)\IIS Express";
+}
+if($path -eq $null)
+{
+    throw "No IIS express installed";
+}
+Start-Process "http://localhost:9090"
+& "$path\iisexpress.exe" /path:E:\projects\about.mbank /port:9090
